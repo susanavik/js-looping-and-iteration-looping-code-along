5ba393d2fd13b5f81a126d7f21f44e207131c345
Susana <susana.m.vik@gmail.com>
2021-04-04 21:56:24 -0400

    commit tree 141d324343365300ad7d082bed991a0af2cf7a9e

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const cards = ["Lisa", "Kaitlin", "Jan"]; 
+
+function writeCards(names) {
+    for (let i = 0; i < cards.length; i++) {
+      console.log(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+    }
+  
+    return cards;
+  }
+
+let countdown = 10;
+
+while (countdown > 0) {
+    console.log(countdown --);
+} 
